e075a07e1fffd4b9602c3bf65130839a0312ff75
Alexey Strelnikov <strelkov@me.com>
2022-05-08 12:09:15 +0300

    Initial Commit

---
+++ skeleton-placeholder/skeleton_placeholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  skeleton_placeholderApp.swift
+//  skeleton-placeholder
+//
+//  Created by Alexey Strelnikov on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct skeleton_placeholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
